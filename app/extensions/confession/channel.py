a3b6d2a72697054d793e14bc456543c6220fcb62
Cayla <selbydarryl05@gmail.com>
2026-08-18 00:11:22 -0400

    sync channel ID

--- app/extensions/confession/channel.py
+++ app/extensions/confession/channel.py
@@ -1,7 +1,7 @@
 import discord
 from discord.ext import commands
 
-CHANNEL_ID = 1525954412424466432
+CHANNEL_ID = 1539107964193873991
 
 async def fetch_channel(bot: commands.Bot | discord.Client):
     return await bot.fetch_channel(CHANNEL_ID)
